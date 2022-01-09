8d87b373aa639e3192882541c3ac86354c8b0dae
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2022-01-10 05:42:46 +0900

    【ADD】ブログの雛形

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { client } from "../libs/client";
+import { Flex } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+
+
+export default function Home({ blog }) {
+  return (
+    
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
